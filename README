207a7cc803df32896af9b2dcf254cf16c8720658
Student Elemente de Informatica Mobila 2016 <student@eim2016>
2016-02-26 03:32:13 +0200

    initial commit

---
+++ README
@@ -0,0 +1,1 @@
+A simple application displaying a dialog in which the user is asked to provide his/her name in order to be properly greeted to the EIM laboratory.
---
+++ labtasks/androidstudio/MyFirstAndroidApplication/app/src/main/java/ro/pub/cs/systems/eim/lab01/myfirstandroidapplication/MainActivity.java
@@ -0,0 +1,66 @@
+package ro.pub.cs.systems.eim.lab01.myfirstandroidapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    final private static long TRANSPARENCY_EFFECT_DURATION = 5000;
+
+    private ButtonClickListener buttonClickListener = new ButtonClickListener();
+
+    private class ButtonClickListener implements Button.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            EditText userNameEditText = (EditText)findViewById(R.id.user_name_edit_text);
+            TextView greetingTextView = (TextView)findViewById(R.id.greeting_text_view);
+            greetingTextView.setAlpha(1);
+
+            // TODO: exercise 9a
+
+            // TODO: exercise 9b
+        }
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Button submitButton = (Button)findViewById(R.id.submit_button);
+        submitButton.setOnClickListener(buttonClickListener);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
